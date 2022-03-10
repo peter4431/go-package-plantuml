df6c8106d2db4ce610f95eed633bbf5737764e91
wyang <peter4431@qq.com>
2022-03-10 16:05:55 +0800

    去掉不必要的 GppathDir 参数;修改 IgnoreDir 为 CodeDir 的相对目录即可;

--- main.go
+++ main.go
@@ -15,9 +15,8 @@ func main() {
 	log.SetLevel(log.InfoLevel)
 
 	var opts struct {
-		CodeDir string `long:"codedir" description:"要扫描的代码目录" required:"true"`
-		GopathDir string `long:"gopath" description:"GOPATH目录" required:"true"`
-		OutputFile string `long:"outputfile" description:"解析结果保存到该文件中" required:"true"`
+		CodeDir    string   `long:"codedir" description:"要扫描的代码目录" required:"true"`
+		OutputFile string   `long:"outputfile" description:"解析结果保存到该文件中" required:"true"`
 		IgnoreDirs []string `long:"ignoredir" description:"需要排除的目录,不需要扫描和解析"`
 	}
 
@@ -35,36 +34,27 @@ func main() {
 
 	if opts.CodeDir == "" {
 		panic("代码目录不能为空")
-		os.Exit(1)
 	}
 
-	if opts.GopathDir == "" {
-		panic("GOPATH目录不能为空")
-		os.Exit(1)
-	}
-
-	if ! strings.HasPrefix(opts.CodeDir, opts.GopathDir) {
-		panic(fmt.Sprintf("代码目录%s,必须是GOPATH目录%s的子目录", opts.CodeDir, opts.GopathDir))
-		os.Exit(1)
-	}
-
+	var newDir string
+	var newIgnoreDirs []string
 	for _, dir := range opts.IgnoreDirs {
-		if ! strings.HasPrefix(dir, opts.CodeDir){
-			panic(fmt.Sprintf("需要排除的目录%s,必须是代码目录%s的子目录", dir, opts.CodeDir))
-			os.Exit(1)
+		if !strings.HasPrefix(dir, opts.CodeDir) {
+			newDir = path.Join(opts.CodeDir, dir)
+		} else {
+			newDir = dir
 		}
+		newIgnoreDirs = append(newIgnoreDirs, newDir)
 	}
 
 	config := codeanalysis.Config{
-		CodeDir: opts.CodeDir,
-		GopathDir :opts.GopathDir,
-		VendorDir : path.Join(opts.CodeDir, "vendor"),
-		IgnoreDirs:opts.IgnoreDirs,
+		CodeDir:    opts.CodeDir,
+		GopathDir:  opts.CodeDir,
+		VendorDir:  path.Join(opts.CodeDir, "vendor"),
+		IgnoreDirs: newIgnoreDirs,
 	}
 
 	result := codeanalysis.AnalysisCode(config)
 
-	result.OutputToFile("/tmp/uml.txt")
-
+	result.OutputToFile(opts.OutputFile)
 }
-
